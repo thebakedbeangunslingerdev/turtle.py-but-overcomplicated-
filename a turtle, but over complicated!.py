faa25f28dad1d4c444790131a29dee8d9832b50d
thebakedbeangunslingerdev <robynpottier87@gmail.com>
2025-09-20 15:45:10 +0200

    Add files via upload

---
+++ a turtle, but over complicated!.py
@@ -0,0 +1,69 @@
+import turtle
+from turtle import *
+
+redguy = turtle.Turtle()
+
+wn = turtle.Screen()
+wn.bgcolor("brown")
+wn.title("red guy attack!")
+
+
+redguy = turtle.Turtle()
+redguy.color("red")
+redguy.pensize(3)
+
+
+redguy.forward(100)
+redguy.left(90)
+redguy.forward(50)
+redguy.forward(50)
+redguy.left(90)
+redguy.right(90)
+redguy.right(90)
+redguy.left(90)
+redguy.right(10)
+redguy.left(90)
+redguy.forward(100)
+redguy.forward(100)
+redguy.left(90)
+redguy.forward(100)
+redguy.left(90)
+redguy.forward(50)
+redguy.down(70)
+redguy.down(70)
+redguy.left(90)
+redguy.left(90)
+redguy.left(90)
+redguy.forward(100)
+
+redguy.forward(100)
+redguy.left(90)
+redguy.forward(50)
+redguy.forward(50)
+redguy.left(90)
+redguy.right(90)
+redguy.right(90)
+redguy.left(90)
+redguy.right(10)
+redguy.left(90)
+redguy.forward(100)
+redguy.forward(100)
+redguy.left(90)
+redguy.forward(100)
+redguy.left(90)
+redguy.forward(50)
+redguy.down(70)
+redguy.down(70)
+redguy.left(90)
+redguy.left(90)
+redguy.left(90)
+redguy.forward(100)
+
+t = turtle.Turtle()
+t.write("Hello, Turtle,we have been wating, we live in the matrix,you know what they say, 101010! stop moving! i say! dont hit me!")
+
+wn.mainloop()
+
+
+
+
